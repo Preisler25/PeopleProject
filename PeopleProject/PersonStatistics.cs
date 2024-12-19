7ce8fcbfa39c17d164aff67131de89ede3aecfdf
Preisler András <90277418+Preisler25@users.noreply.github.com>
2024-12-19 08:48:25 +0100

    Update PersonStatistics.cs

--- PeopleProject/PersonStatistics.cs
+++ PeopleProject/PersonStatistics.cs
@@ -1,28 +1,5 @@
 ﻿namespace PeopleProject
 {
-    /*
-     A PersonStatistics osztály függvényeit C# nyelv esetén nagy betűvel kezdje. Az osztály
-megvalósítása során az alábbiakra ügyeljen.
-a.) Az osztály tárolja az emberek adatait egy people nevű lista adattagban. A lista adattag
-konstruktor paraméterében, valamint setter segítségével is legyen megadható. Csak egy
-konstruktorral rendelkezzen az osztály, amelyben a lista adattag megadása kötelező. A
-lista lehet üres is, viszont null értéket nem vehet fel. A további függvények ebben a
-listában található adatokkal dolgozzanak. Megvalósítási javaslat:
-• C# nyelv esetén People nevű property, melynek settere publikus, gettere privát.
-Külön adattag létrehozása nem kötelező, használhatóak a C# nyelvi elemei.
-• Java nyelv esetén people privát adattag létrehozása és hozzá publikus setPeople
-függvény létrehozása (lombok használható)
-• JavaScript / TypeScript esetén people privát adattag létrehozása és hozzá publikus
-set property létrehozása.
-b.) getAverageAge() – adja vissza az összes személy átlagéletkorát
-c.) getNumberOfStudents() – Adja vissza a tanulók számát.
-d.) getPersonWithHighestScore() – Adja a legtöbb ponttal rendelkező személyt
-e.) getAverageScoreOfStudents() – Adja vissza a tanulók átlag pontszámát
-f.) getOldestStudent()– Adja vissza a legidősebb tanulót
-g.) isAnyoneFailing()– Adja vissza hogy van-e olyan személy, aki megbukott a teszten
-(kevesebb mint 40 pontot ért el)
-     */
-
     public class PersonStatistics
     {
         private List<Person> people;
